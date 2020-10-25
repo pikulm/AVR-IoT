70939b225ee4c0f4b758f432450bb09fea39c71b
Magdalena Pikul <magdalena.anna.pikul@gmail.com>
2020-10-25 15:20:05 +0100

    editing pwm functionality

--- main.c
+++ main.c
@@ -25,15 +25,11 @@
 #include "mcc_generated_files/include/port.h"
 #include "pwm.h"
 
-#define PD3 3	// blue led (WIFI)
 #define PD4 4	// pwm pin on mikroBUS header
 
 int main(void)
 {
     application_init();
-    PORTD_set_pin_dir(PD3, PORT_DIR_OUT);
-	PORTD_set_pin_level(PD3, false);
-	PORTMUX.TCAROUTEA |= (PORTMUX_TCA00_bm | PORTMUX_TCA01_bm);
 
 	PORTD_set_pin_dir(PD4, PORT_DIR_OUT);
 	PORTD_set_pin_level(PD4, false);
@@ -42,9 +38,7 @@ int main(void)
 
 	/* Replace with your application code */
 	easyPWM_init(SYSCLK_DIV_64);
-	easyPWM_load_duty_cycle_ch3(0x80);
 	easyPWM_load_duty_cycle_ch4(0x80);
-	easyPWM_enable_output_ch3();
 	easyPWM_enable_output_ch4();
 	uint16_t duty_cycle = 0x00;
   
@@ -53,7 +47,6 @@ int main(void)
       runScheduler();  
       	for (int i=0; i<1000; i++)
 		for (int j=0; j<100; j++);
-		easyPWM_load_duty_cycle_ch3(duty_cycle);
 		easyPWM_load_duty_cycle_ch4(duty_cycle);
 		duty_cycle += 0x10;
    }
